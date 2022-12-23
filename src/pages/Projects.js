18f618aa489119e56e844553977909ec7fb9357d
Edward Yuabov <yuaboved@gmail.com>
2022-12-23 14:55:15 -0500

    added changes

---
+++ src/pages/Projects.js
@@ -0,0 +1,27 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div id="project-display">
+        <section className="projects">
+          <img />
+          <p>BlackJack</p>
+        </section>
+        <section className="projects">
+          <img />
+          <p>Marvel Cinematic Universe</p>
+        </section>
+        <section className="projects">
+          <img />
+          <p>Blog</p>
+        </section>
+        <section className="projects">
+          <img />
+          <p>Hogwarts</p>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
