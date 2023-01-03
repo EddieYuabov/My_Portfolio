db32908e530555221ade746fa32184eb83a83007
Edward Yuabov <yuaboved@gmail.com>
2023-01-02 22:31:02 -0500

    added screenshots and styling to projects component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,29 +1,34 @@
 const Projects = () => {
   return (
     <div>
-      <h1>Projects</h1>
       <div id="project-display">
         <a
           href="https://parallel-holiday.surge.sh/"
           target="_blank"
           rel="noopener noreferrer"
+          className="link"
         >
           <section className="projects">
-            <img />
+            <img
+              src="https://i.imgur.com/zkYFhj1.png"
+              alt="blackjack picture"
+              id="img"
+            />
             <p>BlackJack</p>
           </section>
         </a>
-        <section className="projects">
+        {/* <section className="projects">
           <img />
           <p>Marvel Cinematic Universe</p>
-        </section>
+        </section> */}
         <a
           href="https://still-wave-91765.herokuapp.com"
           target="_blank"
           rel="noopener noreferrer"
+          className="link"
         >
           <section className="projects">
-            <img />
+            <img src="https://i.imgur.com/xcxtukb.png" alt="blog" id="img" />
             <p>Blog</p>
           </section>
         </a>
@@ -31,9 +36,14 @@ const Projects = () => {
           href="https://salty-taiga-60643.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
+          className="link"
         >
           <section className="projects">
-            <img />
+            <img
+              src="https://i.imgur.com/LtriviY.png"
+              alt="hogwarts"
+              id="img"
+            />
             <p>Hogwarts</p>
           </section>
         </a>
