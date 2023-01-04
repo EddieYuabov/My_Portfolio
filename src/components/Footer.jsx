5d91225a89a899e2ab872222aa046a657f5a4a72
Edward Yuabov <yuaboved@gmail.com>
2023-01-04 14:13:14 -0500

    did css for footer tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
     <div>
         <div id="footer-tags">
             <a href="https://www.linkedin.com/in/edward-yuabov/" target="_blank" rel="noopener noreferrer">
-                <img class="footer-img-tag" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>    
+                <img class="footer-linkedin" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>    
             </a>
             <a href="https://github.com/EddieYuabov" target="_blank" rel="noopener noreferrer">
-                <img class="footer-img-tag" src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"/>    
+                <img class="footer-github" src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"/>    
             </a>
         </div>
         <p id="text-footer">©2023 Edward Yuabov</p>
