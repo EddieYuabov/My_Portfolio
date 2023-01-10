2b5aa6ea615faa658cca5fb8b9023d299e89f415
Edward Yuabov <yuaboved@gmail.com>
2023-01-10 15:04:44 -0500

    styled app for mobile use

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,16 @@ const Home = () => {
       <h2>Full-Stack Developer</h2>
       <h4>Valley Stream, NY</h4>
       <br />
-      <p>I'm a full stack developer who is always eager to learn more.</p>
-      <p>
+      <p className="about-p">
+        I'm a full stack developer who is always eager to learn more.
+      </p>
+      <p className="about-p">
         Within 3 short months I've managed to transform myself into a
         web-developer with almost no prior experience
       </p>
-      <p>In my spare time I play piano, restore cars and program</p>
+      <p className="about-p">
+        In my spare time I play piano, restore cars and program
+      </p>
     </div>
   )
 }
