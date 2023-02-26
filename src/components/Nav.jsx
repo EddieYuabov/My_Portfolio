fdccc5b5d5cee92a9381a556dd7042622c0cbab7
Edward Yuabov <yuaboved@gmail.com>
2023-02-26 16:28:21 -0500

    fixed nav padding hover

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,14 +6,14 @@ const Nav = () => {
             <h2 id="name">Edward Yuabov</h2>
             <h2 id="title">Software Engineer</h2>
             <div id="links">
-                <Link to = '/'><h3 className="nav-font">Home</h3></Link>
-                <Link to = '/projects'><h3 className="nav-font">Projects</h3></Link>
+                <Link to = '/'><h3 id="nav-home-font">Home</h3></Link>
+                <Link to = '/projects'><h3 id="nav-projects-font">Projects</h3></Link>
                 <a 
                 href="https://docs.google.com/document/d/1DVT0-fNo3kYleiR-_dJPom5PQoEmey-wAqZJ0GNS3_4/edit?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                    <h3 className="nav-font">Resume</h3>
+                    <h3 id="nav-resume-font">Resume</h3>
                 </a>
             </div>
         </div>
