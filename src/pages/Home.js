d1af6f6056103952ae109ba9d8dd0bec05e081ea
Edward Yuabov <yuaboved@gmail.com>
2023-02-27 13:09:06 -0500

    added padding to about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,27 @@ const Home = () => {
       <h2 id="header-font">Full-Stack Developer</h2>
       <h4>Valley Stream, NY</h4>
       <br />
-      <p id="about-p">
-        I'm a full stack developer with a passion to learn new things.
-        <br />
-        <br />
-        I am proficient in JavaScript, Python, C++ as well as many other
-        languages and frameworks.
-        <br />
-      </p>
-      <div id="click-bar-display">
+      <section id="about-section">
         <p id="about-p">
-          In my spare time I enjoy playing the piano, working on cars, and
-          developing various computer science programs.
-          <img
-            id="click-bar"
-            alt=""
-            src="https://static.wixstatic.com/media/34168b_d90dce54c59542ffa482c91aacbb6a32~mv2.gif"
-          />
+          I'm a full stack developer with a passion to learn new things.
+          <br />
+          <br />
+          I am proficient in JavaScript, Python, C++ as well as many other
+          languages and frameworks.
+          <br />
         </p>
-      </div>
+        <div id="click-bar-display">
+          <p id="about-p">
+            In my spare time I enjoy playing the piano, working on cars, and
+            developing various computer science programs.
+            <img
+              id="click-bar"
+              alt=""
+              src="https://static.wixstatic.com/media/34168b_d90dce54c59542ffa482c91aacbb6a32~mv2.gif"
+            />
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
