a2a26460d25efb99c9f4f7904933bb8ff7c5647b
Edward Yuabov <yuaboved@gmail.com>
2023-02-27 02:27:43 -0500

    added text cursor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,10 +13,18 @@ const Home = () => {
         <br />
         and frameworks
         <br />
-        <br />
-        In my spare time I enjoy playing the piano, working on cars, and
-        developing various computer science programs
       </p>
+      <div id="click-bar-display">
+        <p id="about-p">
+          In my spare time I enjoy playing the piano, working on cars, and
+          developing various computer science programs.
+          <img
+            id="click-bar"
+            alt=""
+            src="https://static.wixstatic.com/media/34168b_d90dce54c59542ffa482c91aacbb6a32~mv2.gif"
+          />
+        </p>
+      </div>
     </div>
   )
 }
